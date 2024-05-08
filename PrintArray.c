5b31b60f4e29bfe09fe39df5b3e41eb98f9a2d1c
sumanmanna-03 <mannasuman189@gmail.com>
2024-05-08 08:31:41 +0530

    printing array
    done

---
+++ PrintArray.c
@@ -0,0 +1,16 @@
+// printing 2D array
+
+#include <stdio.h>
+int main()
+{
+  int arr[2][2] = {{1, 2}, {3, 4}};
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      printf("%d  ",arr[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
